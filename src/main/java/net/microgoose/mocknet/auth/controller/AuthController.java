96a5cd86061062be0279d635aa8177411d98214b
microgoose <gusakmachine@yandex.ru>
2025-10-20 13:25:46 +0300

    feat(auth): добавлен logout

--- src/main/java/net/microgoose/mocknet/auth/controller/AuthController.java
+++ src/main/java/net/microgoose/mocknet/auth/controller/AuthController.java
@@ -11,10 +11,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -24,9 +21,15 @@ public class AuthController {
     private final AuthService authService;
     private final TokenCookieService tokenCookieService;
 
-    // TODO logout
     // TODO password recovery
     // TODO disable/ban user
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout() {
+        return ResponseEntity.status(HttpStatus.OK)
+            .header(HttpHeaders.SET_COOKIE, tokenCookieService.createExpiredRefreshTokenCookie().toString())
+            .build();
+    }
 
     @PostMapping("/login")
     public ResponseEntity<AuthResponse> login(@RequestBody LoginRequest request) {
