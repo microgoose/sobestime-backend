410d8e1e810a8102c32737ed18e012a170b32e96
microgoose <gusakmachine@yandex.ru>
2025-10-20 13:47:55 +0300

    fix: добавлен бин аутен. менеджера

--- src/main/java/net/microgoose/mocknet/auth/config/SecurityConfig.java
+++ src/main/java/net/microgoose/mocknet/auth/config/SecurityConfig.java
@@ -6,8 +6,6 @@ import net.microgoose.mocknet.auth.security.JwtAuthenticationProvider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -46,10 +44,4 @@ public class SecurityConfig {
             .build();
     }
 
-    @Bean
-    public AuthenticationManager authenticationManager(
-        AuthenticationConfiguration authenticationConfiguration) throws Exception {
-
-        return authenticationConfiguration.getAuthenticationManager();
-    }
 }
