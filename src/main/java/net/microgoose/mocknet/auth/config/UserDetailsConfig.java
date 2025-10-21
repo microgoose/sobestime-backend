3bf512321382138567cf094f49146d2410855864
microgoose <gusakmachine@yandex.ru>
2025-10-21 16:39:20 +0300

    feat: совмещение сервисов извлечения пользователя из БД

--- src/main/java/net/microgoose/mocknet/auth/config/UserDetailsConfig.java
+++ src/main/java/net/microgoose/mocknet/auth/config/UserDetailsConfig.java
@@ -1,7 +1,7 @@
 package net.microgoose.mocknet.auth.config;
 
 import lombok.RequiredArgsConstructor;
-import net.microgoose.mocknet.auth.security.DbUserDetailsService;
+import net.microgoose.mocknet.auth.service.UserPrincipalService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,11 +12,11 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @RequiredArgsConstructor
 public class UserDetailsConfig {
 
-    private final DbUserDetailsService dbUserDetailsService;
+    private final UserPrincipalService userPrincipalService;
 
     @Bean
     public UserDetailsService userDetailsService() {
-        return dbUserDetailsService;
+        return userPrincipalService;
     }
 
     @Bean
--- src/main/java/net/microgoose/mocknet/auth/service/UserPrincipalService.java
+++ src/main/java/net/microgoose/mocknet/auth/service/UserPrincipalService.java
@@ -7,9 +7,14 @@ import net.microgoose.mocknet.auth.dto.RegistrationRequest;
 import net.microgoose.mocknet.auth.mapper.UserPrincipalMapper;
 import net.microgoose.mocknet.auth.model.Role;
 import net.microgoose.mocknet.auth.model.UserPrincipal;
+import net.microgoose.mocknet.auth.repository.RoleRepository;
 import net.microgoose.mocknet.auth.repository.UserPrincipalRepository;
+import net.microgoose.mocknet.auth.security.UserDetailsImpl;
 import net.microgoose.mocknet.intermediate.dto.UserRegisterEvent;
 import net.microgoose.mocknet.intermediate.topic.UserTopic;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -18,12 +23,21 @@ import static net.microgoose.mocknet.auth.config.MessageDictionary.*;
 
 @Service
 @RequiredArgsConstructor
-public class UserPrincipalService {
+public class UserPrincipalService implements UserDetailsService {
 
     private final UserPrincipalRepository repository;
+    private final RoleRepository roleRepository;
     private final UserPrincipalMapper mapper;
     private final RoleService roleService;
     private final UserTopic userTopic;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        UserPrincipal userPrincipal = repository.findByEmail(email)
+            .orElseThrow(() -> new UsernameNotFoundException(USER_NOT_FOUND));
+        userPrincipal.setRoles(roleRepository.findByUsersContains(userPrincipal));
+        return new UserDetailsImpl(userPrincipal);
+    }
 
     public UserPrincipal getUserByEmail(String email) {
         return repository.findByEmail(email)
